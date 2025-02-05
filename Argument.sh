3f77c94f035111f9a90659bf48a25cd65ace7a08
hilalbasibuyuk <hilal.basibuyuk21@ogr.atauni.edu.tr>
2025-02-05 11:03:20 +0300

    Some accessing methods for command line argument

---
+++ Argument.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Script Name : "$0;
+echo "All Arguments : "$*;
+echo "Number Of Arguments :" $#;
+echo "First Argument : "$1;
+echo "Second Argument : "$2;
+echo "Third Argument : "$3;
+
+# Argümanlı olarak çalıştırırız. Mesela, "./Argument.sh Deniz Dal BMS301"
